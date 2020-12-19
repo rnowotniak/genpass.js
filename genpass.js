8c1744245197d13544995ed6b5fbe0f24de6d7be
Robert Nowotniak <rnowotniak@gmail.com>
2020-12-20 00:05:32 +0100

    The first working fine version

--- genpass.js
+++ genpass.js
@@ -4,27 +4,72 @@
 //
 'use strict';
 
-
+// Load modules
 const path = require('path')
+const { ArgumentParser } = require('argparse');
 
+// Default password generator settings
+const DEFAULT_LENGTH = 8
+const DEFAULT_SYMBOLS = false
+const DEFAULT_UPPER = false
+const DEFAULT_NUMBERS = false
 
+// Symbols table to use (needs enabling with DEFAULT_SYMBOLS boolean)
+const SYMBOLS_TABLE = '~`!@#$%^&*()_+-={}|[]\\;\':",./<>?'
 
-//hello('robert')
+/**
+ * Generate a random password per settings given in the parameters.
+ * By default 'ab...z' set is used to randomly choose from.
 
+ * @param  {Number}  settings.length   The length of password
+ * @param  {Boolean} settings.upper    Whether to use upper case chars additionally
+ * @param  {Boolean} settings.symbols  Whether to use symbols additionally
+ * @param  {Boolean} settings.numbers  Whether to use numbers additionally
+ *
+ * @return {String}                    The generated password
+ */
+function generatePassword(settings = {}) {
+	const { length=4, upper=false, symbols=false, numbers=false } = settings
 
-function usage() {
-	console.log("Usage:")
-	console.log("\t" + path.basename(process.argv[1]) + " [ -l|--length N ]")
+	const aCode = 'a'.charCodeAt(0)
+	const zCode = 'z'.charCodeAt(0)
+
+	// 'ab...z' string by default
+	let charsSet = String.fromCharCode(
+		...[...[...'_'.repeat(zCode - aCode + 1)].keys()].map(c => c+aCode))
+	if (upper) {
+		// append 'AB...Z'
+		charsSet += charsSet.toUpperCase()
+	}
+	if (numbers) {
+		// append '01...9'
+		charsSet += [...Array(10).keys()].join('')
+	}
+	if (symbols) {
+		// append SYMBOLS_TABLE
+		charsSet += SYMBOLS_TABLE
+	}
+
+	let result = [...'_'.repeat(length)]
+		.map(() => charsSet[Math.floor(Math.random()*charsSet.length)])
+		.join('')
+	
+	return result
 }
 
-// console.log(process.argv0)
-// console.log(process.argv)
-console.log(process.argv.slice(2))
 
-if (process.argv.length == 2) {
-	usage();
-	process.exit(1);
-}
+const parser = new ArgumentParser({description:"Password generator in JavaScript"});
+parser.add_argument('--length', '-l', { type:'int', help:'Password length'})
+parser.add_argument('--symbols', '-s', { action:'store_true', help:'Include symbols'} )
+parser.add_argument('--upper', '-u', { action:'store_true', help:'Include upper case letters'} )
+parser.add_argument('--numbers', '-n', { action:'store_true', help:'Include numbers'} )
+let args = parser.parse_args()
 
-process.exit(0)
+let length = args.length ? args.length : DEFAULT_LENGTH
+let symbols = args.symbols ? args.symbols : DEFAULT_SYMBOLS
+let upper = args.upper ? args.upper : DEFAULT_UPPER
+let numbers = args.numbers ? args.numbers : DEFAULT_NUMBERS
+
+let password = generatePassword({length, symbols, upper, numbers})
+console.log(password)
 
